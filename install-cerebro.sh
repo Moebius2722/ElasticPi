965c1878f8799e2e9fb63a83cf84e7baafeab876
Moebius2722 <renaud.vorbe@laposte.net>
2016-12-17 15:17:29 +0100

    Add installation script for Cerebro

---
+++ install-cerebro.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# Author : Moebius2722
+# Mail : moebius2722@laposte.net
+
+# Full Automated Installation Script for Cerebro on Raspberry Pi 2 or 3
+
+
+####### COMMON #######
+
+# Set Version Cerebro
+C_VERSION=0.4.1
+
+####### CEREBRO #######
+
+# Get and Install Cerebro
+wget -P/tmp https://github.com/lmenezes/cerebro/releases/download/v${C_VERSION}/cerebro-${C_VERSION}.tgz && sudo tar -xf /tmp/cerebro-${C_VERSION}.tgz -C /usr/share && sudo mv /usr/share/cerebro-${C_VERSION} /usr/share/cerebro
+
+# Create Cerebro Group
+if ! getent group cerebro >/dev/null; then
+  sudo groupadd -r cerebro
+fi
+
+# Create Cerebro User
+if ! getent passwd cerebro >/dev/null; then
+  sudo useradd -M -r -g cerebro -d /usr/share/cerebro -s /usr/sbin/nologin -c "Cerebro Service User" cerebro
+fi
+
+sudo chown -R cerebro:cerebro /usr/share/cerebro
+
+# Configure and Start Cerebro as Daemon
+sudo cp -f ./Cerebro/cerebro.service /etc/systemd/system/.
