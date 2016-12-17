ca577f300aadeb1a4dbd05e9db99a0cd6c8ebbba
Moebius2722 <renaud.vorbe@laposte.net>
2016-12-17 11:27:48 +0100

    Add script for overclock Pi 3

---
+++ overclock-pi3.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Author : Moebius2722
+# Mail : moebius2722@laposte.net
+
+# Hard Overcloking Raspberry Pi 3
+
+echo arm_freq=1400 | sudo tee -a /boot/config.txt
+echo sdram_freq=500 | sudo tee -a /boot/config.txt
+echo core_freq=500 | sudo tee -a /boot/config.txt
+echo over_voltage=6 | sudo tee -a /boot/config.txt
