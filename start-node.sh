5321abe0e2bab5a3918785d4d2abebf04d8ed1fd
Moebius2722 <renaud.vorbe@laposte.net>
2017-02-07 16:22:01 +0100

    Add Start Node Script

---
+++ start-node.sh
@@ -0,0 +1,134 @@
+#!/bin/bash
+
+# Author : Moebius2722
+# Mail : moebius2722@laposte.net
+# Git : https://github.com/Moebius2722/ElkPi.git
+
+# Full Automated Start Node Services Script for Elasticsearch on Raspberry Pi 2 or 3
+
+
+####### COMMON #######
+
+# Get IP Node
+if [[ $# = 0 ]] ; then
+  ipnode=`hostname -i`
+else
+  ipnode=$1
+fi
+
+
+####### START-NODE #######
+
+# Check and Start Node Services
+
+# Start Elasticsearch
+echo "================================= Elasticsearch ================================"
+ssh $ipnode sudo systemctl enable elasticsearch.service >/dev/null 2>/dev/null
+ssh $ipnode sudo systemctl status elasticsearch.service >/dev/null 2>/dev/null
+if [[ ! $? = 0 ]] ; then
+  echo "$ipnode : Start Elasticsearch"
+  ssh $ipnode sudo systemctl start elasticsearch.service >/dev/null 2>/dev/null
+fi
+
+# Wait for start-up nodes
+echo "Wait for start-up nodes"
+b_check=0
+int_cpt=0
+while [ $b_check -eq 1 -a $int_cpt -lt 120 ]; do
+  b_check=`curl -ss -XGET 'localhost:9200/_cat/health?pretty'|cut -d ' ' -f 4|grep -ci red`
+  echo -n '.'
+  sleep 5
+  int_cpt=$[$int_cpt+1]
+done
+echo
+if [ $b_check -eq 1 -a $int_cpt -eq 120 ]; then
+  echo "Time Out for start-up nodes"
+else
+  # Reenable shard allocation
+  echo "Reenable shard allocation"
+  curl -XPUT 'localhost:9200/_cluster/settings?pretty' -H 'Content-Type: application/json' -d'
+  {
+    "transient": {
+      "cluster.routing.allocation.enable": "all"
+    }
+  }
+  ' >/dev/null 2>/dev/null
+fi
+
+# Wait for the node to recover
+echo "Wait for the nodes to recover"
+b_check=0
+int_cpt=0
+while [ $b_check -eq 0 -a $int_cpt -lt 180 ]; do
+  b_check=`curl -ss -XGET 'localhost:9200/_cat/health?pretty'|cut -d ' ' -f 4|grep -ci green`
+  echo -n '.'
+  sleep 10
+  int_cpt=$[$int_cpt+1]
+done
+echo
+if [ $b_check -eq 0 -a $int_cpt -eq 180 ]; then
+  echo "Time Out for the node to recover."
+fi
+
+# Start Cerebro
+echo "==================================== Cerebro ==================================="
+ssh $ipnode sudo systemctl enable cerebro.service >/dev/null 2>/dev/null
+ssh $ipnode sudo systemctl status cerebro.service >/dev/null 2>/dev/null
+if [[ ! $? = 0 ]] ; then
+  echo "$ipnode : Start Cerebro"
+  ssh $ipnode sudo systemctl start cerebro.service >/dev/null 2>/dev/null
+fi
+
+# Start Mosquitto
+echo "=================================== Mosquitto =================================="
+ssh $ipnode sudo systemctl enable mosquitto.service >/dev/null 2>/dev/null
+ssh $ipnode sudo systemctl status mosquitto.service >/dev/null 2>/dev/null
+if [[ ! $? = 0 ]] ; then
+  echo "$ipnode : Start Mosquitto"
+  ssh $ipnode sudo systemctl start mosquitto.service >/dev/null 2>/dev/null
+fi
+
+# Start Node-RED
+echo "=================================== Node-RED ==================================="
+ssh $ipnode sudo systemctl enable nodered.service >/dev/null 2>/dev/null
+ssh $ipnode sudo systemctl status nodered.service >/dev/null 2>/dev/null
+if [[ ! $? = 0 ]] ; then
+  echo "$ipnode : Start Node-RED"
+  ssh $ipnode sudo systemctl start nodered.service >/dev/null 2>/dev/null
+fi
+
+# Start Logstash
+echo "=================================== Logstash ==================================="
+ssh $ipnode sudo systemctl enable logstash.service >/dev/null 2>/dev/null
+ssh $ipnode sudo systemctl status logstash.service >/dev/null 2>/dev/null
+if [[ ! $? = 0 ]] ; then
+  echo "$ipnode : Start Logstash"
+  ssh $ipnode sudo systemctl start logstash.service >/dev/null 2>/dev/null
+fi
+
+# Start Kibana
+echo "==================================== Kibana ===================================="
+ssh $ipnode sudo systemctl enable kibana.service >/dev/null 2>/dev/null
+ssh $ipnode sudo systemctl status kibana.service >/dev/null 2>/dev/null
+if [[ ! $? = 0 ]] ; then
+  echo "$ipnode : Start Kibana"
+  ssh $ipnode sudo systemctl start kibana.service >/dev/null 2>/dev/null
+fi
+
+# Start Nginx
+echo "===================================== Nginx ===================================="
+ssh $ipnode sudo systemctl enable nginx.service >/dev/null 2>/dev/null
+ssh $ipnode sudo systemctl status nginx.service >/dev/null 2>/dev/null
+if [[ ! $? = 0 ]] ; then
+  echo "$ipnode : Start Nginx"
+  ssh $ipnode sudo systemctl start nginx.service >/dev/null 2>/dev/null
+fi
+
+# Start Keepalived
+echo "================================== Keepalived =================================="
+ssh $ipnode sudo systemctl enable keepalived.service >/dev/null 2>/dev/null
+ssh $ipnode sudo systemctl status keepalived.service >/dev/null 2>/dev/null
+if [[ ! $? = 0 ]] ; then
+  echo "$ipnode : Start Keepalived"
+  ssh $ipnode sudo systemctl start keepalived.service >/dev/null 2>/dev/null
+fi
