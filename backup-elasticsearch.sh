68e7a4b0b6b118f332fa93ca779683fe1f6c3603
Moebius2722 <renaud.vorbe@laposte.net>
2017-02-09 10:09:37 +0100

    Add Elasticsearch Backup Script

---
+++ backup-elasticsearch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Author : Moebius2722
+# Mail : moebius2722@laposte.net
+# Git : https://github.com/Moebius2722/ElkPi.git
+
+# Full Automated Backup Script for Elasticsearch on Raspberry Pi 2 or 3
+
+
+####### BACKUP #######
+
+allssh "sudo -H -u elasticsearch /usr/local/bin/curator --config /etc/elasticsearch/curator-config.yml /etc/elasticsearch/curator-actions.yml"
