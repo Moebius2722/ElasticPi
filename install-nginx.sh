93bb754a31ffc936644d3ab548d2443d82d2c45f
Moebius2722 <renaud.vorbe@laposte.net>
2017-02-11 17:00:15 +0100

    Install with Stream option in Official Nginx package

--- install-nginx.sh
+++ install-nginx.sh
@@ -19,8 +19,19 @@ fi
 
 ####### NGINX #######
 
+# Get Auth PAM module
+git clone https://github.com/sto/ngx_http_auth_pam_module.git /tmp/ngx_http_auth_pam_module
+
 # Install Nginx Reverse Proxy
-sudo apt-get install nginx -q -y
+#sudo apt-get install nginx-extras -q -y
+sudo apt-get purge nginx nginx-common nginx-doc nginx-extras nginx-extras-dbg nginx-full nginx-full-dbg nginx-light nginx-light-dbg -q -y
+sudo apt-get install libpcre3 libpcre3-dev libperl5.20 libperl-dev zlib1g zlib1g-dev libssl-dev libssl1.0.0 libxml2 libxml2-dev libxslt1.1 libxslt1-dev libjpeg62-turbo libjpeg62-turbo-dev libgd3 libgd-dev libgeoip1 libgeoip-dev libgoogle-perftools4 libgoogle-perftools-dev libpam0g libpam0g-dev -q -y
+wget -P/tmp http://nginx.org/download/nginx-1.11.9.tar.gz && tar -xzvf /tmp/nginx-1.11.9.tar.gz -C /tmp && cd /tmp/nginx-1.11.9
+./configure --prefix=/var/www --sbin-path=/usr/sbin/nginx --conf-path=/etc/nginx/nginx.conf --pid-path=/run/nginx.pid --error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access.log --user=www-data --group=www-data --with-threads --with-file-aio --with-ipv6 --with-http_ssl_module --with-http_v2_module --with-http_realip_module --with-http_addition_module --with-http_xslt_module --with-http_image_filter_module --with-http_geoip_module --with-http_sub_module --with-http_dav_module --with-http_flv_module --with-http_gunzip_module --with-http_gzip_static_module --with-http_auth_request_module --with-http_random_index_module --with-http_secure_link_module --with-http_slice_module --with-http_degradation_module --with-http_stub_status_module --with-http_perl_module --with-mail --with-mail_ssl_module --with-stream --with-stream_ssl_module --with-google_perftools_module --with-cpp_test_module --with-debug --add-module=/tmp/ngx_http_auth_pam_module
+make
+sudo make install
+
+sudo apt-get -o Dpkg::Options::="--force-overwrite" -o Dpkg::Options::="--force-confnew" install nginx-common -q -y
 
 # Create SSL Auto Signed Certificate for Nginx Reverse Proxy
 sudo mkdir /etc/nginx/ssl && (echo FR; echo France; echo Paris; echo Raspberry Pi; echo Nginx; echo $(hostname -I); echo ;) | sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/nginx.key -out /etc/nginx/ssl/nginx.crt
