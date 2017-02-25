68f92c488279f6b56f05688aca5fe4fdc42e0d1b
Moebius2722 <renaud.vorbe@laposte.net>
2017-02-25 15:00:02 +0100

    Add Update Tools Script

---
+++ update-tools.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Author : Moebius2722
+# Mail : moebius2722@laposte.net
+# Git : https://github.com/Moebius2722/ElasticPi.git
+
+# Full Automated Update Script for ElasticPi Tools on Raspberry Pi 2 or 3
+
+
+####### TOOLS #######
+
+allssh "rm -rf ~/ElasticPi ; git clone https://github.com/Moebius2722/ElasticPi.git ~/ElasticPi ; chown -R a+x ~/ElasticPi/*.sh"
