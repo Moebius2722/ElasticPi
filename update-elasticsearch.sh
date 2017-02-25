1e68ce9d1ab384e2533af4f9a3ce3a0a5cf1d892
Moebius2722 <renaud.vorbe@laposte.net>
2017-02-25 19:11:58 +0100

    Fix Wait Loop

--- update-elasticsearch.sh
+++ update-elasticsearch.sh
@@ -63,39 +63,41 @@ sudo /bin/systemctl start elasticsearch.service
 # Install and Configure Curator for Elasticsearch
 sudo pip install --upgrade PySocks && sudo pip install --upgrade elasticsearch-curator
 
-# Wait for start-up the upgraded node
-echo "Wait for start-up the upgraded node."
-SHORTHOSTNAME=`hostname -s`
-b_check=0
+# Wait for start-up nodes
+echo "Wait for start-up Elasticsearch nodes"
+s_check=red
 int_cpt=0
-while [ $b_check -eq 0 -a $int_cpt -lt 120 ]; do
-  b_check=`curl -XGET 'localhost:9200/_cat/nodes?pretty'|grep -ic $SHORTHOSTNAME`
+while [ "$s_check" != "yellow" ] && [ "$s_check" != "green" ] && [ $int_cpt -lt 120 ]; do
+  s_check=`curl -ss -XGET 'localhost:9200/_cat/health?pretty'|cut -d ' ' -f 4`
+  echo -n '.'
   sleep 5
   int_cpt=$[$int_cpt+1]
 done
-if [ $b_check -eq 0 -a $int_cpt -eq 120 ]; then
-  echo "Time Out for start-up the upgraded node."
+echo
+if [ "$s_check" != "yellow" ] && [ "$s_check" != "green" ] && [ $int_cpt -eq 120 ]; then
+  echo "Time Out for start-up nodes"
 else
-# Reenable shard allocation
-echo "Reenable shard allocation."
-curl -XPUT 'localhost:9200/_cluster/settings?pretty' -H 'Content-Type: application/json' -d'
-{
-  "transient": {
-    "cluster.routing.allocation.enable": "all"
+  # Reenable shard allocation
+  echo "Reenable shard allocation"
+  curl -XPUT 'localhost:9200/_cluster/settings?pretty' -H 'Content-Type: application/json' -d'
+  {
+    "transient": {
+      "cluster.routing.allocation.enable": "all"
+    }
   }
-}
-'
+  ' >/dev/null 2>/dev/null
 fi
 
-# Wait for the node to recover
-echo "Wait for the node to recover."
-b_check=0
+# Wait for the nodes to recover
+echo "Wait for the Elasticsearch nodes to recover"
 int_cpt=0
-while [ $b_check -eq 0 -a $int_cpt -lt 180 ]; do
-  b_check=`curl -XGET 'localhost:9200/_cat/health?pretty'|cut -d ' ' -f 4|grep -ci green`
+while [ "$s_check" != "green" ] && [ $int_cpt -lt 180 ]; do
+  s_check=`curl -ss -XGET 'localhost:9200/_cat/health?pretty'|cut -d ' ' -f 4`
+  echo -n '.'
   sleep 10
   int_cpt=$[$int_cpt+1]
 done
-if [ $b_check -eq 0 -a $int_cpt -eq 180 ]; then
+echo
+if [ "$s_check" != "green" ] && [ $int_cpt -eq 180 ]; then
   echo "Time Out for the node to recover."
 fi
