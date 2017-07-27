f47060f9ac0fda8ce60d30b654f809e6c8ca7dd8
Moebius2722 <renaud.vorbe@laposte.net>
2017-07-27 20:13:10 +0200

    Add Update Elastic-HQ Script

---
+++ update-elastichq.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Author : Moebius2722
+# Mail : moebius2722@laposte.net
+# Git : https://github.com/Moebius2722/ElasticPi.git
+
+# Full Automated Update Script for Elastic-HQ on Raspberry Pi 2 or 3 to Secured Kibana
+
+
+####### ELASTIC-HQ #######
+
+# Install Elastic-HQ
+cd /opt/elastichq ; sudo git pull
