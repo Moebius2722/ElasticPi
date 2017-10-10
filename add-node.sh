7d5a4ec76526d6984f64b23f95c8f8fece14d1ec
Moebius2722 <renaud.vorbe@laposte.net>
2017-10-10 21:49:23 +0200

    Finalize add-node command.

--- add-node.sh
+++ add-node.sh
@@ -22,41 +22,29 @@ ipnode=$1
 nodepwd=$2
 
 # Install sshpass utility for ssh connection without password prompt
-sudo apt-get install sshpass -q -y >/dev/null
+allssh "sudo apt-get install sshpass -q -y >/dev/null"
 
 
 ####### CONFIGURE NODE #######
-
-echo 'Node ===========================' $ipnode '==========================='
+echo '=========================== Add Node ' $ipnode '==========================='
 
 # Remove old IP node in SSH known_hosts
 allssh "ssh-keygen -R $ipnode >/dev/null 2>/dev/null"
-  
+
 # Add new IP node in SSH known_hosts
 allssh "ssh-keyscan -H $ipnode >> ~/.ssh/known_hosts 2>/dev/null"
-
-# Get Node Short Name
-namenode=`sshpass -p $nodepwd ssh -t $ipnode 'hostname -s'`
-
-# Remove old Short Name node in SSH known_hosts
-allssh "ssh-keygen -R $namenode >/dev/null 2>/dev/null"
-
-# Add new Short Name node in SSH known_hosts
-allssh "ssh-keyscan -H $namenode >> ~/.ssh/known_hosts 2>/dev/null"
-
-# Get Node Long Name
-fqdnnode=`sshpass -p $nodepwd ssh -t $ipnode 'hostname -f'`
-
-# Remove old Long Name node in SSH known_hosts
-allssh "ssh-keygen -R $fqdnnode >/dev/null 2>/dev/null"
-
-# Add new Long Name node in SSH known_hosts
-allssh "ssh-keyscan -H $fqdnnode >> ~/.ssh/known_hosts 2>/dev/null"
 
 # Generate RSA Key for SSH Auto Connect
 sshpass -p $nodepwd ssh -t $ipnode 'sudo apt-get install sshpass -q -y >/dev/null ; rm -f ~/.ssh/authorized_keys ; rm -f ~/.ssh/id_rsa ; rm -f ~/.ssh/id_rsa.pub ; ssh-keygen -q -t rsa -N "" -f ~/.ssh/id_rsa >/dev/null 2>/dev/null'
 
 # Add Node Autorizations
-allssh "ssh-copy-id $ipnode >/dev/null 2>/dev/null"
-allssh "ssh-copy-id $namenode >/dev/null 2>/dev/null"
-allssh "ssh-copy-id $fqdnnode >/dev/null 2>/dev/null"
+allssh "sshpass -p $nodepwd ssh-copy-id $ipnode >/dev/null 2>/dev/null"
+
+# Add Node to Cluster
+echo $ipnode | sudo tee -a /etc/elasticpi/nodes.lst >/dev/null 2>/dev/null
+scp /etc/elasticpi/nodes.lst $ipnode:/tmp/nodes.lst >/dev/null 2>/dev/null
+ssh $ipnode "sudo mkdir /etc/elasticpi && sudo cp /tmp/nodes.lst /etc/elasticpi/nodes.lst && rm /tmp/nodes.lst >/dev/null 2>/dev/null"
+
+# Install Cluster Tools on New Node
+scp /opt/elasticpi/install-tools.sh $ipnode:/tmp/install-tools.sh
+ssh $ipnode "/bin/bash /tmp/install-tools.sh >/dev/null 2>/dev/null"
