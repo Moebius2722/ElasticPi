a7b28da0afd74e55858c3584176fd263bf941c04
Moebius2722 <renaud.vorbe@laposte.net>
2017-10-23 16:45:07 +0200

    Change SSH Keys by Unique Cluster Key.

--- add-node.sh
+++ add-node.sh
@@ -11,12 +11,12 @@
 
 # Check Parameters
 if [[ ! $# = 2 ]] ; then
-  echo "Usage : $0 IpNode NodePassword"
+  echo "Usage : $0 IpNewNode NodePassword"
   exit 1
 fi
 
 # Get IP Node
-ipnode=$1
+ipnewnode=$1
 
 # Get Node Password
 nodepwd=$2
@@ -26,25 +26,32 @@ allssh "sudo apt-get install sshpass -q -y >/dev/null"
 
 
 ####### CONFIGURE NODE #######
-echo '=========================== Add Node ' $ipnode '==========================='
+echo '=========================== Add Node ' $ipnewnode '==========================='
 
 # Remove old IP node in SSH known_hosts
-allssh "ssh-keygen -R $ipnode >/dev/null 2>/dev/null"
+allssh "ssh-keygen -R $ipnewnode >/dev/null 2>/dev/null"
 
 # Add new IP node in SSH known_hosts
-allssh "ssh-keyscan -H $ipnode >> ~/.ssh/known_hosts 2>/dev/null"
+allssh "ssh-keyscan -H $ipnewnode >> ~/.ssh/known_hosts 2>/dev/null"
 
 # Generate RSA Key for SSH Auto Connect
-sshpass -p $nodepwd ssh -t $ipnode 'sudo apt-get install sshpass -q -y >/dev/null ; rm -f ~/.ssh/authorized_keys ; rm -f ~/.ssh/id_rsa ; rm -f ~/.ssh/id_rsa.pub ; ssh-keygen -q -t rsa -N "" -f ~/.ssh/id_rsa >/dev/null 2>/dev/null'
+#sshpass -p $nodepwd ssh -t $ipnewnode 'sudo apt-get install sshpass -q -y >/dev/null ; rm -f ~/.ssh/authorized_keys ; rm -f ~/.ssh/id_rsa ; rm -f ~/.ssh/id_rsa.pub ; ssh-keygen -q -t rsa -N "" -f ~/.ssh/id_rsa >/dev/null 2>/dev/null'
 
 # Add Node Autorizations
-allssh "sshpass -p $nodepwd ssh-copy-id $ipnode >/dev/null 2>/dev/null"
+#allssh "sshpass -p $nodepwd ssh-copy-id -f $ipnewnode >/dev/null 2>/dev/null"
+#ssh $ipnewnode "ssh-keygen -R $ipnewnode >/dev/null 2>/dev/null"
+#ssh $ipnewnode "ssh-keyscan -H $ipnewnode >> ~/.ssh/known_hosts 2>/dev/null"
+#ssh $ipnewnode "sshpass -p $nodepwd ssh-copy-id -f $ipnewnode >/dev/null 2>/dev/null"
+sshpass -p $nodepwd scp ~/.ssh/id_rsa 192.168.0.23:~/.ssh/id_rsa
+sshpass -p $nodepwd scp ~/.ssh/id_rsa.pub 192.168.0.23:~/.ssh/id_rsa.pub
+sshpass -p $nodepwd scp ~/.ssh/authorized_keys  192.168.0.23:~/.ssh/authorized_keys
+sshpass -p $nodepwd scp ~/.ssh/known_hosts  192.168.0.23:~/.ssh/known_hosts
 
 # Add Node to Cluster
-echo $ipnode | sudo tee -a /etc/elasticpi/nodes.lst >/dev/null 2>/dev/null
-scp /etc/elasticpi/nodes.lst $ipnode:/tmp/nodes.lst >/dev/null 2>/dev/null
-ssh $ipnode "sudo mkdir /etc/elasticpi && sudo cp /tmp/nodes.lst /etc/elasticpi/nodes.lst && rm /tmp/nodes.lst >/dev/null 2>/dev/null"
+allssh "echo $ipnewnode | sudo tee -a /etc/elasticpi/nodes.lst >/dev/null 2>/dev/null"
+scp /etc/elasticpi/nodes.lst $ipnewnode:/tmp/nodes.lst >/dev/null 2>/dev/null
+ssh $ipnewnode "sudo mkdir /etc/elasticpi && sudo cp /tmp/nodes.lst /etc/elasticpi/nodes.lst && rm /tmp/nodes.lst >/dev/null 2>/dev/null"
 
 # Install Cluster Tools on New Node
-scp /opt/elasticpi/install-tools.sh $ipnode:/tmp/install-tools.sh
-ssh $ipnode "/bin/bash /tmp/install-tools.sh >/dev/null 2>/dev/null"
+scp /opt/elasticpi/install-tools.sh $ipnewnode:/tmp/install-tools.sh
+ssh $ipnewnode "/bin/bash /tmp/install-tools.sh >/dev/null 2>/dev/null"
