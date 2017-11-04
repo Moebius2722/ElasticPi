7b52c543dbd612bc87d7af869b54cf9296875b38
Moebius2722 <renaud.vorbe@laposte.net>
2017-11-04 22:46:04 +0100

    Check installation

--- install-elasticsearch.sh
+++ install-elasticsearch.sh
@@ -15,6 +15,12 @@ if [ ! -f /etc/elasticpi/nodes.lst ]; then
   exit 1
 fi
 
+# Check if already installed
+if get-elasticsearch-version >/dev/null 2>/dev/null; then
+  echo "Elasticsearch is already installed" >&2
+  exit 1
+fi
+
 # Get IP Host
 iphost=`hostname -I | cut -d ' ' -f 1`
 
