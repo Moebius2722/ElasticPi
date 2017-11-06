39d216a002d41ee2b1e2e73107d62193efd4d091
Moebius2722 <renaud.vorbe@laposte.net>
2017-11-06 22:48:41 +0100

    Display Progressive

--- check-cluster.sh
+++ check-cluster.sh
@@ -22,56 +22,64 @@ ipnodes=( `sudo cat /etc/elasticpi/nodes.lst | grep -e '^[0-9]*\.[0-9]*\.[0-9]*\
 
 for ipnode in "${ipnodes[@]}"
 do
+  printf " $ipnode"
+  ilen=${#ipnode}
+  iend=$((17-ilen))
+  printf "%${iend}s"
   ssh -t $ipnode check-elasticsearch >/dev/null 2>/dev/null
   if [[ $? = 0 ]] ; then
     ses=ok
   else
     ses=KO
   fi
+  printf "$ses "
   ssh -t $ipnode check-logstash >/dev/null 2>/dev/null
   if [[ $? = 0 ]] ; then
     sls=ok
   else
     sls=KO
   fi
+  printf "$sls "
   ssh -t $ipnode check-kibana >/dev/null 2>/dev/null
   if [[ $? = 0 ]] ; then
     skb=ok
   else
     skb=KO
   fi
+  printf "$skb "
   ssh -t $ipnode check-nginx >/dev/null 2>/dev/null
   if [[ $? = 0 ]] ; then
     sng=ok
   else
     sng=KO
   fi
+  printf "$sng "
   ssh -t $ipnode check-cerebro >/dev/null 2>/dev/null
   if [[ $? = 0 ]] ; then
     scb=ok
   else
     scb=KO
   fi
+  printf "$scb "
   ssh -t $ipnode check-nodered >/dev/null 2>/dev/null
   if [[ $? = 0 ]] ; then
     snr=ok
   else
     snr=KO
   fi
+  printf "$snr "
   ssh -t $ipnode check-mosquitto >/dev/null 2>/dev/null
   if [[ $? = 0 ]] ; then
     smq=ok
   else
     smq=KO
   fi
+  printf "$smq "
   ssh -t $ipnode check-keepalived >/dev/null 2>/dev/null
   if [[ $? = 0 ]] ; then
     ska=ok
   else
     ska=KO
   fi
-  iipnode=${#ipnode}
-  iend=$((15-iipnode))
-  send=`printf "%${iend}s"`
-  echo " $ipnode$send  $ses $sls $skb $sng $scb $snr $smq $ska"
+  printf "$ska\n"
 done
