408419711a59cae3e8b6147e31602b4bb401369d
Moebius2722 <renaud.vorbe@laposte.net>
2017-11-06 22:55:34 +0100

    Display Progressive

--- get-cluster-version.sh
+++ get-cluster-version.sh
@@ -22,65 +22,87 @@ ipnodes=( `sudo cat /etc/elasticpi/nodes.lst | grep -e '^[0-9]*\.[0-9]*\.[0-9]*\
 
 for ipnode in "${ipnodes[@]}"
 do
-  echo $ipnode
+  printf " $ipnode"
+  ilen=${#ipnode}
+  iend=$((17-ilen))
+  printf "%${iend}s"
   ves=`ssh -t $ipnode get-elasticsearch-version 2>/dev/null`
   if [[ $? = 0 ]] ; then
     ses=ok
   else
     ves="N/A"
   fi
-  echo $ves
+  printf $ves
+  ilen=${#ves}
+  iend=$((9-ilen))
+  printf "%${iend}s"
   vls=`ssh -t $ipnode get-logstash-version 2>/dev/null`
   if [[ $? = 0 ]] ; then
     sls=ok
   else
     vls="N/A"
   fi
-  echo $vls
+  printf $vls
+  ilen=${#vls}
+  iend=$((9-ilen))
+  printf "%${iend}s"
   vkb=`ssh -t $ipnode get-kibana-version 2>/dev/null`
   if [[ $? = 0 ]] ; then
     skb=ok
   else
     vkb="N/A"
   fi
-  echo $vkb
+  printf $vkb
+  ilen=${#vkb}
+  iend=$((9-ilen))
+  printf "%${iend}s"
   vng=`ssh -t $ipnode get-nginx-version 2>/dev/null`
   if [[ $? = 0 ]] ; then
     sng=ok
   else
     vng="N/A"
   fi
-  echo $vng
+  printf $vng
+  ilen=${#vng}
+  iend=$((9-ilen))
+  printf "%${iend}s"
   vcb=`ssh -t $ipnode get-cerebro-version 2>/dev/null`
   if [[ $? = 0 ]] ; then
     scb=ok
   else
     vcb="N/A"
   fi
-  echo $vcb
+  printf $vcb
+  ilen=${#vcb}
+  iend=$((9-ilen))
+  printf "%${iend}s"
   vnr=`ssh -t $ipnode get-nodered-version 2>/dev/null`
   if [[ $? = 0 ]] ; then
     snr=ok
   else
     vnr="N/A"
   fi
-  echo $vnr
+  printf $vnr
+  ilen=${#vnr}
+  iend=$((9-ilen))
+  printf "%${iend}s"
   vmq=`ssh -t $ipnode get-mosquitto-version 2>/dev/null`
   if [[ $? = 0 ]] ; then
     smq=ok
   else
     vmq="N/A"
   fi
-  echo $vmq
+  printf $vmq
+  ilen=${#vmq}
+  iend=$((9-ilen))
+  printf "%${iend}s"
   vka=`ssh -t $ipnode get-keepalived-version 2>/dev/null`
   if [[ $? = 0 ]] ; then
     ska=ok
   else
     vka="N/A"
   fi
-  echo $vka
-  iipnode=${#ipnode}
-  iend=$((15-iipnode))
-  send=`printf "%${iend}s"`
-  echo " $ipnode$send  $ves $vls $vkb $vng $vcb $vnr $vmq $vka"
+  printf $vka
+  printf "\n"
+  
 done
