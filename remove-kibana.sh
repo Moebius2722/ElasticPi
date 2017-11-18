e9084c04a1e184d0f36a6460c0b3d38c33e22a13
Moebius2722 <renaud.vorbe@laposte.net>
2017-11-18 12:38:14 +0100

    Add Remove Kibana Script

---
+++ remove-kibana.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Author : Moebius2722
+# Mail : moebius2722@laposte.net
+# Git : https://github.com/Moebius2722/ElasticPi.git
+
+# Full Automated Uninstallation Script for Kibana on Raspberry Pi 2 or 3
+
+
+####### KIBANA #######
+
+# Stop Kibana
+stop-kibana
+
+# Remove Kibana
+sudo dpkg --purge kibana
+
+# Purge Kibana configuration
+sudo rm -rf /etc/kibana
+sudo rm -rf /var/lib/kibana
+sudo rm -rf /var/log/kibana
