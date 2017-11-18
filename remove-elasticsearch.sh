bb8665d0eb032ba64b5ade4fad516350fc2242f1
Moebius2722 <renaud.vorbe@laposte.net>
2017-11-18 09:01:20 +0100

    Add Remove Elasticsearch Script

---
+++ remove-elasticsearch.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Author : Moebius2722
+# Mail : moebius2722@laposte.net
+# Git : https://github.com/Moebius2722/ElasticPi.git
+
+# Full Automated Uninstallation Script for Elasticsearch on Raspberry Pi 2 or 3
+
+
+####### COMMON #######
+
+# Remove Curator Job
+sudo sed -i '/curator/d' /etc/crontab
+sudo /bin/systemctl restart cron.service
+sudo rm -rf /var/log/curator
+
+# Uninstall Curator
+sudo pip uninstall elasticsearch-curator -y
+
+# Stop Elasticsearch
+stop-elasticsearch
+
+# Remove Elasticsearch
+sudo dpkg --purge elasticsearch
+
+# Purge Elasticsearch configuration
+sudo rm -rf /etc/elasticsearch
+
+# Remove Elasticsearch Backup NFS mount point
+sudo sed -i '/espibackup/d' /etc/fstab
+sudo umount /mnt/espibackup
+sudo rm -rf /mnt/espibackup
