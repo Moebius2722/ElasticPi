74adffa8e0478f590fb8c5dc205906b0f1674f63
Moebius2722 <renaud.vorbe@laposte.net>
2017-12-06 07:50:44 +0100

    Add Get Metricbeat Last Verstion Command

---
+++ get-metricbeat-lastversion.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Author : Moebius2722
+# Mail : moebius2722@laposte.net
+# Git : https://github.com/Moebius2722/ElasticPi.git
+
+# Get Metricbeat last version
+
+
+####### GET-METRICBEAT-VERSION #######
+
+# Get Metricbeat last version
+
+wget https://www.elastic.co/downloads/beats/metricbeat/ -qO- | grep -i "\-amd64\.deb\" class=\"zip-link\">" | cut -d '"' -f 2 | cut -d / -f 7 | cut -d - -f 2 | cut -d . -f 1-3 | head -n 1
