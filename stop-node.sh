900feabb3c383d0477dda09a384c6f89539f2ab8
Moebius2722 <renaud.vorbe@laposte.net>
2018-01-05 19:45:00 +0100

    Optimized Script

--- stop-node.sh
+++ stop-node.sh
@@ -7,73 +7,35 @@
 # Full Automated Stop Node Services Script for Elasticsearch on Raspberry Pi 2 or 3
 
 
-####### COMMON #######
-
-# Get IP Node
-if [[ $# = 0 ]] ; then
-  ipnode=`hostname -i`
-else
-  ipnode=$1
-fi
-
-
 ####### STOP-NODE #######
 
 # Check and Stop Node Services
 echo "================================== STOP-NODE ==================================="
 date
 
-# Stop Elasticsearch
-echo "================================= Elasticsearch ================================"
-# Disable shard allocation
-echo "Disable shard allocation"
-curl -XPUT 'localhost:9200/_cluster/settings?pretty' -H 'Content-Type: application/json' -d'
-{
-  "transient": {
-    "cluster.routing.allocation.enable": "none"
-  }
-}
-' >/dev/null 2>/dev/null
-
-sleep 10
-
-# Flush Index and Stop Elasticsearch Services
-ssh -t $ipnode sudo systemctl status elasticsearch.service >/dev/null 2>/dev/null
+# Flush Elasticsearch Indices
+ssh -t $1 sudo systemctl status elasticsearch.service >/dev/null 2>/dev/null
 if [[ $? = 0 ]] ; then
-  # Exclude node of shard allocation
-  #ssh -t $ipnode "curl -XPUT 'localhost:9200/_cluster/settings?pretty' -H 'Content-Type: application/json' -d'
-  #{
-  #  \"transient\": {
-  #    \"cluster.routing.allocation.exclude._ip\" : \"$ipnode\"
-  #  }
-  #}
-  #'
-  #" >/dev/null 2>/dev/null
-  ## Wait for node entering in maintenance mode
-  #echo "Wait for node entering in maintenance mode"
-  #nb_shards=1
-  #int_cpt=0
-  #while [ "$nb_shards" != "0" ] && [ $int_cpt -lt 120 ]; do
-  #  nb_shards=`curl -ss -XGET 'localhost:9200/_cat/shards'|grep -ic "$ipnode"`
-  #  echo -n '.'
-  #  sleep 5
-  #  int_cpt=$[$int_cpt+1]
-  #done
-  #echo
-  echo "$ipnode : Stop elasticsearch"
-  ssh -t $ipnode "curl -XPOST 'localhost:9200/_flush/synced?pretty'" >/dev/null 2>/dev/null
+  echo "Flush Elasticsearch Indices"
+  if [[ $# = 0 ]] ; then
+    curl -XPOST 'localhost:9200/_flush/synced?pretty'
+  else
+    ssh -t $1 "curl -XPOST 'localhost:9200/_flush/synced?pretty'"
+  fi
 fi
-ssh -t $ipnode stop-elasticsearch >/dev/null 2>/dev/null
-
 
 # Stop Services
-for svc in logstash kibana nodered mosquitto cerebro keepalived nginx         
+for svc in elasticsearch logstash kibana nodered mosquitto cerebro keepalived nginx         
 do
 echo "================================= $svc ================================"
-echo "$ipnode : Stop $svc"
-ssh -t $ipnode stop-$svc >/dev/null 2>/dev/null
+echo "$1 : Stop $svc"
+  if [[ $? = 0 ]] ; then
+    stop-$svc
+  else
+    ssh -t $1 stop-$svc
+  fi
 done
-
 
 date
 echo "================================== NODE-STOP ==================================="
+#	>/dev/null 2>/dev/null
