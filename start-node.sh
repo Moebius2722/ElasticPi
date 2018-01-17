0290fb5e3ea9fdac6446340f6299266643306e2a
Moebius2722 <renaud.vorbe@laposte.net>
2018-01-17 18:42:12 +0100

    Fix Start Other Node

--- start-node.sh
+++ start-node.sh
@@ -36,6 +36,9 @@ fi
 
 # Waiting Elasticsearch Up
 echo "==================================== Waiting ==================================="
+
+if [[ $# = 0 ]] ; then
+
 # Wait for start-up nodes
 echo "Wait for start-up Elasticsearch nodes"
 s_check=red
@@ -63,6 +66,39 @@ done
 echo
 if [ "$s_check" != "green" ] && [ $int_cpt -eq 180 ]; then
   echo "Time Out for the node to recover."
+fi
+
+else
+
+# Wait for start-up nodes
+echo "Wait for start-up Elasticsearch nodes"
+s_check=red
+int_cpt=0
+while [ "$s_check" != "yellow" ] && [ "$s_check" != "green" ] && [ $int_cpt -lt 120 ]; do
+  s_check=`curl -ss -XGET "$1:9200/_cat/health?pretty"|cut -d ' ' -f 4`
+  echo -n '.'
+  sleep 5
+  int_cpt=$[$int_cpt+1]
+done
+echo
+if [ "$s_check" != "yellow" ] && [ "$s_check" != "green" ] && [ $int_cpt -eq 120 ]; then
+  echo "Time Out for start-up nodes"
+fi
+
+# Wait for the nodes to recover
+echo "Wait for the Elasticsearch nodes to recover"
+int_cpt=0
+while [ "$s_check" != "green" ] && [ $int_cpt -lt 180 ]; do
+  s_check=`curl -ss -XGET "$1:9200/_cat/health?pretty"|cut -d ' ' -f 4`
+  echo -n '.'
+  sleep 10
+  int_cpt=$[$int_cpt+1]
+done
+echo
+if [ "$s_check" != "green" ] && [ $int_cpt -eq 180 ]; then
+  echo "Time Out for the node to recover."
+fi
+
 fi
 
 date
