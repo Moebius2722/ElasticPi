5ec109a8c1696a9ef43630c6acdcccb599f7cf02
Moebius2722 <renaud.vorbe@laposte.net>
2018-02-04 22:22:28 +0100

    Replace Tab by Space

--- create-squidguard-blacklist.sh
+++ create-squidguard-blacklist.sh
@@ -4,17 +4,17 @@ SQUIDLIB=/var/lib/squidguard/db
 SQUIDLIB_BLACKLISTS=$SQUIDLIB"/blacklists"
 
 if [ -d $SQUIDLIB_BLACKLISTS ]; then
-	for folderName in `ls $SQUIDLIB_BLACKLISTS`; do
-		if [ -d "$SQUIDLIB_BLACKLISTS/$folderName" ]; then
-			echo "dest $folderName {"
-			if [ -e "$SQUIDLIB_BLACKLISTS/$folderName/domains" ]; then
-				echo "      domainlist blacklists/$folderName/domains"
-			fi
-			if [ -e "$SQUIDLIB_BLACKLISTS/$folderName/urls" ]; then
-				echo "      urllist blacklists/$folderName/urls"
-			fi
-			echo "      log ${folderName}accesses"
-			echo "}"
-		fi
-	done
+    for folderName in `ls $SQUIDLIB_BLACKLISTS`; do
+        if [ -d "$SQUIDLIB_BLACKLISTS/$folderName" ]; then
+            echo "dest $folderName {"
+            if [ -e "$SQUIDLIB_BLACKLISTS/$folderName/domains" ]; then
+                echo "      domainlist blacklists/$folderName/domains"
+            fi
+            if [ -e "$SQUIDLIB_BLACKLISTS/$folderName/urls" ]; then
+                echo "      urllist blacklists/$folderName/urls"
+            fi
+            echo "      log ${folderName}accesses"
+            echo "}"
+        fi
+    done
 fi
