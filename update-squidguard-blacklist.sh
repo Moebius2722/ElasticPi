b075516603dca51aedce45bc039ce4d8abc6bb6e
Moebius2722 <renaud.vorbe@laposte.net>
2018-02-04 22:21:58 +0100

    Add Refresh Cache

--- update-squidguard-blacklist.sh
+++ update-squidguard-blacklist.sh
@@ -11,4 +11,4 @@
 
 #Update Squidguard Blacklist
 
-rm -f /tmp/blacklists.tar.gz ; wget -P/tmp http://dsi.ut-capitole.fr/blacklists/download/blacklists.tar.gz && sudo rm -rf /var/lib/squidguard/db/blacklists && sudo tar -xzvf /tmp/blacklists.tar.gz -C /var/lib/squidguard/db && sudo squidGuard -C all && sudo chown -R proxy:proxy /var/lib/squidguard/db && sudo systemctl restart squid.service
+rm -f /tmp/blacklists.tar.gz ; wget -P/tmp http://dsi.ut-capitole.fr/blacklists/download/blacklists.tar.gz && sudo rm -rf /var/lib/squidguard/db/blacklists && sudo tar -xzvf /tmp/blacklists.tar.gz -C /var/lib/squidguard/db && sudo systemctl stop squid.service && sudo squidGuard -C all && sudo chown -R proxy:proxy /var/lib/squidguard/db && sudo squid3 -z && sudo systemctl start squid.service
