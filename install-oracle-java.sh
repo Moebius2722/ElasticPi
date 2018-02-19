08b39ccbd32f38ea3a8299abeb98268dd9578947
Moebius2722 <renaud.vorbe@laposte.net>
2018-02-19 22:57:17 +0100

    Add Oracle Java Installer

---
+++ install-oracle-java.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Author : Moebius2722
+# Mail : moebius2722@laposte.net
+# Git : https://github.com/Moebius2722/ElasticPi.git
+
+# Full Automated Installation Script for Oracle Java on Raspberry Pi 2 or 3
+
+
+####### COMMON #######
+
+# Full System Update
+if [[ ! "${PI_UPDATED}" = "1" ]]; then
+  echo "Full System Update"
+  sudo apt-get update && sudo apt-get upgrade -q -y && sudo apt-get dist-upgrade -q -y && sudo rpi-update
+  export PI_UPDATED=1
+fi
+
+
+####### ORACLE-JAVA #######
+
+# Install apt-key Prerequisites
+sudo apt-get install dirmngr -q -y
+
+echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | sudo tee /etc/apt/sources.list.d/webupd8team-java.list
+echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | sudo tee -a /etc/apt/sources.list.d/webupd8team-java.list
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886
+sudo apt-get update
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+sudo apt-get install oracle-java8-set-default oracle-java8-installer -q -y --allow-unauthenticated
