d90beb076b107a4da6615cc4f8ae99918167799b
Moebius2722 <renaud.vorbe@laposte.net>
2018-02-19 21:25:35 +0100

    Add requirement

--- install-keepalived.sh
+++ install-keepalived.sh
@@ -43,7 +43,7 @@ sudo sysctl -w net.ipv4.conf.lo.arp_announce=2
 sudo sysctl -p
 
 # Install Keepalived Load Balancer
-sudo apt-get install libipset3 keepalived -q -y
+sudo apt-get install libipset3 netcat keepalived -q -y
 
 # Configure Keepalived Load Balancer
 configure-keepalived
