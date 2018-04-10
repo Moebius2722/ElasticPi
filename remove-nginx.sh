cee6367b1d39efbad48e26946dd13454328f5245
Moebius2722 <renaud.vorbe@laposte.net>
2018-04-10 23:51:11 +0200

    Add Remove Nginx Script

---
+++ remove-nginx.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Author : Moebius2722
+# Mail : moebius2722@laposte.net
+# Git : https://github.com/Moebius2722/ElasticPi.git
+
+# Full Automated Uninstallation Script for Nginx on Raspberry Pi 2 or 3
+
+
+####### NGINX #######
+
+# Stop Nginx
+stop-nginx
+
+# Remove Nginx Daemon
+sudo apt-get purge nginx-common -q -y
+sudo /bin/systemctl daemon-reload
+
+# Remove Nginx
+sudo rm -rf /etc/nginx
+sudo rm -rf /var/www
+sudo rm -rf /var/log/nginx
+sudo rm -f /usr/sbin/nginx
+sudo rm -f /run/nginx.pid
