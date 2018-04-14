5af23981ca96706cd0e512ac495e2715fc10c4b6
Moebius2722 <renaud.vorbe@laposte.net>
2018-04-14 17:49:18 +0200

    Fix Rights

--- create-repo.sh
+++ create-repo.sh
@@ -11,8 +11,10 @@
 
 # Create and configure Repo NFS mount point
 sudo mkdir /mnt/elasticpi
+sudo chmod -R 770 /mnt/elasticpi
 sudo apt-get install nfs-common -q -y
 sudo systemctl enable rpcbind.service
 sudo systemctl start rpcbind.service
 echo '192.168.0.1:/volume1/elasticpi /mnt/elasticpi nfs rw         0       0' | sudo tee -a /etc/fstab
 sudo mount /mnt/elasticpi
+sudo chmod -R 770 /mnt/elasticpi
