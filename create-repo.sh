8b63f6a0b6aa919adc14c1be3b6a923d77b0a39f
Moebius2722 <renaud.vorbe@laposte.net>
2018-04-14 17:42:48 +0200

    Add Create and Remove NFS Repo Scripts

---
+++ create-repo.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Author : Moebius2722
+# Mail : moebius2722@laposte.net
+# Git : https://github.com/Moebius2722/ElasticPi.git
+
+# Create Repo NFS Mount Folder for Elastic Stack on Raspberry Pi 2 or 3
+
+
+####### REPO #######
+
+# Create and configure Repo NFS mount point
+sudo mkdir /mnt/elasticpi
+sudo apt-get install nfs-common -q -y
+sudo systemctl enable rpcbind.service
+sudo systemctl start rpcbind.service
+echo '192.168.0.1:/volume1/elasticpi /mnt/elasticpi nfs rw         0       0' | sudo tee -a /etc/fstab
+sudo mount /mnt/elasticpi
---
+++ remove-repo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Author : Moebius2722
+# Mail : moebius2722@laposte.net
+# Git : https://github.com/Moebius2722/ElasticPi.git
+
+# Remove Repo NFS Mount Folder for Elastic Stack on Raspberry Pi 2 or 3
+
+
+####### REPO #######
+
+# Remove and Unconfigure Repo NFS mount point
+sudo umount -f /mnt/elasticpi
+allssh sudo sed -i "/\/mnt\/elasticpi/d" /etc/fstab
+sudo rm -rf /mnt/elasticpi
