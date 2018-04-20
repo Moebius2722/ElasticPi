e8883aa47b5b8cb2e98cfbc00d08bf7a2567eab1
Moebius2722 <renaud.vorbe@laposte.net>
2018-04-20 20:54:22 +0200

    Remove "Remove Backup NFS mount point"

--- remove-elasticsearch.sh
+++ remove-elasticsearch.sh
@@ -38,8 +38,3 @@ sudo rm -rf /usr/share/elasticsearch
 sudo rm -rf /var/lib/elasticsearch
 sudo rm -rf /var/log/elasticsearch
 sudo rm -f /etc/sysctl.d/96-elasticsearch.conf
-
-# Remove Elasticsearch Backup NFS mount point
-sudo sed -i '/espibackup/d' /etc/fstab
-sudo umount /mnt/espibackup
-sudo rm -rf /mnt/espibackup
