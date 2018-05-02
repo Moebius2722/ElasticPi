7281fc1ff63ced1615869acba813fcbc1df7b102
Moebius2722 <renaud.vorbe@laposte.net>
2018-05-02 19:59:42 +0200

    Fix Display Error

--- start-cluster.sh
+++ start-cluster.sh
@@ -7,12 +7,6 @@
 # Full Automated Start Cluster Services Script for Elasticsearch on Raspberry Pi 2 or 3
 
 
-####### COMMON #######
-
-# Get IP Nodes
-ipnodes=( `sudo cat /etc/elasticpi/nodes.lst | grep -e '^[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*$' | sort` )
-
-
 ####### START-CLUSTER #######
 
 # Check and Start Cluster Services
@@ -24,14 +18,11 @@ date
 for svc in nginx keepalived elasticsearch cerebro metricbeat logstash kibana
 do
 echo "================================= $svc ================================"
-for ipnode in "${ipnodes[@]}"
-do
-echo "$ipnode : Start $svc"
-ssh -t $ipnode start-$svc >/dev/null 2>/dev/null
+allssh start-$svc
 done
-done
-
 
 # Waiting Elasticsearch Up
-wait-elasticsearch-start
+allssh wait-elasticsearch-start
+
+date
 echo "================================= CLUSTER-START ================================"
