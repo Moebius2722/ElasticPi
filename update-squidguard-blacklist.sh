80e34578b18d8547b8813a0e1e22bca8165a2f67
Moebius2722 <renaud.vorbe@laposte.net>
2018-05-24 18:43:06 +0200

    Add Stop/Start Squid.

--- update-squidguard-blacklist.sh
+++ update-squidguard-blacklist.sh
@@ -10,5 +10,6 @@
 ####### UPDATE-SQUIDGUARD-BLACKLIST #######
 
 #Update Squidguard Blacklist
-
-rm -f /tmp/blacklists.tar.gz ; wget -P/tmp http://dsi.ut-capitole.fr/blacklists/download/blacklists.tar.gz && sudo rm -rf /var/lib/squidguard/db/blacklists && sudo tar -xzvf /tmp/blacklists.tar.gz -C /var/lib/squidguard/db && sudo systemctl stop squid.service && sudo squidGuard -C all && sudo chown -R proxy:proxy /var/lib/squidguard/db && sudo squid3 -z && sudo systemctl start squid.service
+stop-squid
+rm -f /tmp/blacklists.tar.gz ; wget -P/tmp http://dsi.ut-capitole.fr/blacklists/download/blacklists.tar.gz && sudo rm -rf /var/lib/squidguard/db/blacklists && sudo tar -xzvf /tmp/blacklists.tar.gz -C /var/lib/squidguard/db && sudo squidGuard -C all && sudo chown -R proxy:proxy /var/lib/squidguard/db && sudo squid3 -z
+start-squid
