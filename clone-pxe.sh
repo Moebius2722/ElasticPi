6e6c23cac9de27569be08ad9e8144e7168dfb85e
Moebius2722 <renaud.vorbe@laposte.net>
2018-08-10 00:03:12 +0200

    Add Clone-PXE Script

---
+++ clone-pxe.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Author : Moebius2722
+# Mail : moebius2722@laposte.net
+# Git : https://github.com/Moebius2722/ElasticPi.git
+
+# Full Automated Clone PXE Install for Raspberry Pi 2 or 3
+
+
+####### CLONE-PXE #######
+
+cpuinfoserial=`cat /proc/cpuinfo | grep -i Serial`
+serialsize=8
+cpuserial=`echo ${cpuinfoserial:${#cpuinfoserial} - $serialsize}`
+
+sudo umount /mnt/tftp
+sudo rm -rf /mnt/tftp
+sudo mkdir /mnt/tftp
+sudo mount -t nfs 192.168.0.1:/volume1/tftp /mnt/tftp
+sudo mkdir /mnt/tftp/$cpuserial
+sudo rsync -ax --progress --exclude */ /mnt/tftp/ /mnt/tftp/$cpuserial
+sudo rsync -ax --progress /mnt/tftp/overlays /mnt/tftp/$cpuserial
+
+sudo umount /mnt/pxe
+sudo rm -rf /mnt/pxe
+sudo mkdir /mnt/pxe
+sudo mount -t nfs 192.168.0.1:/volume1/rpi /mnt/pxe
+sudo mkdir /mnt/pxe/$cpuserial
+sudo rsync -ax --progress  --exclude mnt/* --exclude boot/* --exclude boot/.* --exclude boot.bak/ /mnt/pxe/rpi/ /mnt/pxe/$cpuserial
+
+sudo sed -i "s/\/volume1\/rpi\/rpi/\/volume1\/rpi\/$cpuserial/" /mnt/tftp/$cpuserial/cmdline.txt
+sudo sed -i "s/\/volume1\/tftp/\/volume1\/tftp\/$cpuserial/" /mnt/pxe/$cpuserial/etc/fstab
+
+sudo reboot
