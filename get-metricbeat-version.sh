c2cfa57b07189c3f032b2d7e0293ad91ec6dcc84
Moebius2722 <renaud.vorbe@laposte.net>
2018-08-26 11:34:56 +0200

    Managed OSS and Standard version

--- get-metricbeat-version.sh
+++ get-metricbeat-version.sh
@@ -7,18 +7,23 @@
 # Get Metricbeat installed version
 
 
-####### COMMON #######
+####### GET-METRICBEAT-VERSION #######
+
+# Check if Metricbeat OSS is installed
+
+if dpkg-query --showformat='${source:Upstream-Version}\n' --show metricbeat-oss >/dev/null 2>/dev/null; then
+  # Get Metricbeat installed version
+  dpkg-query --showformat='${source:Upstream-Version}\n' --show metricbeat-oss 2>/dev/null | head -n 1
+  exit 0
+fi
 
 # Check if Metricbeat is installed
 
 if ! dpkg-query --showformat='${source:Upstream-Version}\n' --show metricbeat >/dev/null 2>/dev/null; then
   echo "Metricbeat is not installed" >&2
   exit 1
+else
+  # Get Metricbeat installed version
+  dpkg-query --showformat='${source:Upstream-Version}\n' --show metricbeat 2>/dev/null | head -n 1
+  exit 0
 fi
-
-
-####### GET-METRICBEAT-VERSION #######
-
-# Get Metricbeat installed version
-
-dpkg-query --showformat='${source:Upstream-Version}\n' --show metricbeat 2>/dev/null | head -n 1
