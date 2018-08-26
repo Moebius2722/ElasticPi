07a41c5f9174a5fa2d6f9168319d93ab03cc1ef3
Moebius2722 <renaud.vorbe@laposte.net>
2018-08-26 14:58:19 +0200

    Update

--- backup-node-state.sh
+++ backup-node-state.sh
@@ -10,14 +10,17 @@
 ####### START-CLUSTER #######
 
 # Check and Start Cluster Services
+sudo rm -f /etc/elasticpi/node.state
 echo "================================= BACKUP-NODE-STATE ================================"
 # Start Services
 #for svc in nginx keepalived elasticsearch cerebro mosquitto nodered logstash kibana
 for svc in nginx keepalived squid elasticsearch cerebro kibana logstash metricbeat mosquitto nodered
 do
 echo "================================= $svc ================================"
-sudo systemctl is-enabled $svc.service
+sudo systemctl is-enabled $svc.service >/dev/null 2>/dev/null
+if [ $? -eq 0 ] ; then
+  echo $svc | sudo tee -a /etc/elasticpi/node.state
+fi
 done
-
 
 echo "================================= CLUSTER-NODE-BACKUPs ================================"
