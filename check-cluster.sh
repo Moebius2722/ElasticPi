b10c715234b17a347bcc85da99cfdc3dec803791
Moebius2722 <renaud.vorbe@laposte.net>
2019-01-07 09:19:27 +0100

    Add Color State

--- check-cluster.sh
+++ check-cluster.sh
@@ -8,6 +8,12 @@
 
 
 ####### COMMON #######
+RED='\033[0;31m' # RED Color
+GREEN='\033[0;32m' # GREEN Color
+NC='\033[0m'     # No Color
+NA=${NA}
+KO="${RED}KO${NC}"
+OK="${GREEN}ok${NC}"
 
 echo " HOST             ES LS KB MB NG CB NR MQ KA SQ"
 
@@ -29,101 +35,101 @@ do
   ssh -t $ipnode check-elasticsearch >/dev/null 2>/dev/null
   ees=$?
   if [[ $ees = 0 ]] ; then
-    ses=ok
+    ses=${OK}
   elif [[ $ees = 4 ]] ; then
-    ses='  '
+    ses=${NA}
   else
-    ses=KO
+    ses=${KO}
   fi
   printf "$ses "
   ssh -t $ipnode check-logstash >/dev/null 2>/dev/null
   els=$?
   if [[ $els = 0 ]] ; then
-    sls=ok
+    sls=${OK}
   elif [[ $els = 4 ]] ; then
-    sls='  '
+    sls=${NA}
   else
-    sls=KO
+    sls=${KO}
   fi
   printf "$sls "
   ssh -t $ipnode check-kibana >/dev/null 2>/dev/null
   ekb=$?
   if [[ $ekb = 0 ]] ; then
-    skb=ok
+    skb=${OK}
   elif [[ $ekb = 4 ]] ; then
-    skb='  '
+    skb=${NA}
   else
-    skb=KO
+    skb=${KO}
   fi
   printf "$skb "
   ssh -t $ipnode check-metricbeat >/dev/null 2>/dev/null
   emb=$?
   if [[ $emb = 0 ]] ; then
-    smb=ok
+    smb=${OK}
   elif [[ $emb = 4 ]] ; then
-    smb='  '
+    smb=${NA}
   else
-    smb=KO
+    smb=${KO}
   fi
   printf "$smb "
   ssh -t $ipnode check-nginx >/dev/null 2>/dev/null
   eng=$?
   if [[ $eng = 0 ]] ; then
-    sng=ok
+    sng=${OK}
   elif [[ $eng = 4 ]] ; then
-    sng='  '
+    sng=${NA}
   else
-    sng=KO
+    sng=${KO}
   fi
   printf "$sng "
   ssh -t $ipnode check-cerebro >/dev/null 2>/dev/null
   ecb=$?
   if [[ $ecb = 0 ]] ; then
-    scb=ok
+    scb=${OK}
   elif [[ $ecb = 4 ]] ; then
-    scb='  '
+    scb=${NA}
   else
-    scb=KO
+    scb=${KO}
   fi
   printf "$scb "
   ssh -t $ipnode check-nodered >/dev/null 2>/dev/null
   enr=$?
   if [[ $enr = 0 ]] ; then
-    snr=ok
+    snr=${OK}
   elif [[ $enr = 4 ]] ; then
-    snr='  '
+    snr=${NA}
   else
-    snr=KO
+    snr=${KO}
   fi
   printf "$snr "
   ssh -t $ipnode check-mosquitto >/dev/null 2>/dev/null
   emq=$?
   if [[ $emq = 0 ]] ; then
-    smq=ok
+    smq=${OK}
   elif [[ $emq = 4 ]] ; then
-    smq='  '
+    smq=${NA}
   else
-    smq=KO
+    smq=${KO}
   fi
   printf "$smq "
   ssh -t $ipnode check-keepalived >/dev/null 2>/dev/null
   eka=$?
   if [[ $eka = 0 ]] ; then
-    ska=ok
+    ska=${OK}
   elif [[ $eka = 4 ]] ; then
-    ska='  '
+    ska=${NA}
   else
-    ska=KO
+    ska=${KO}
   fi
   printf "$ska "
   ssh -t $ipnode check-squid >/dev/null 2>/dev/null
   esq=$?
   if [[ $esq = 0 ]] ; then
-    ssq=ok
+    ssq=${OK}
   elif [[ $esq = 4 ]] ; then
-    ssq='  '
+    ssq=${NA}
   else
-    ssq=KO
+    ssq=${KO}
   fi
   printf "$ssq\n"
 done
